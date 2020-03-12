b50853364f84be978c7c7aef0a3c7944f5c8b408
Ryan Stelzleni <rstelzleni@nvidia.com>
2020-03-12 11:08:56 -0700

    [pxr bin] pre-review notes pass

--- pxr/usd/bin/usdcompress/usdcompress.py
+++ pxr/usd/bin/usdcompress/usdcompress.py
@@ -22,6 +22,9 @@
 # KIND, either express or implied. See the Apache License for the specific
 # language governing permissions and limitations under the Apache License.
 #
+
+from __future__ import print_function
+
 from pxr import Usd
 from pxr import UsdGeom
 from pxr import UsdDraco
