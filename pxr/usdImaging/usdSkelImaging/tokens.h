21b21dbfa667ce909a31dcf03bc6197bc970c081
unhyperbolic <unhyperbolic@users.noreply.github.com>
2025-02-24 16:16:33 -0800

    UsdSkelImaging: refactor for Hydra 2.0 implementation. Moving CPU ext computation from adapter into its own function. This function takes the skinning method and is in its own file. Also adding functions to return data sources for the ext computation. Adding ext computation related tokens to usdSkelImaging/tokens.h.
    (Internal change: 2357869)

--- pxr/usdImaging/usdSkelImaging/tokens.h
+++ pxr/usdImaging/usdSkelImaging/tokens.h
@@ -22,6 +22,47 @@ TF_DECLARE_PUBLIC_TOKENS(
     UsdSkelImagingPrimTypeTokens,
     USDSKELIMAGING_API, USD_SKEL_IMAGING_PRIM_TYPE_TOKENS);
 
+#define USD_SKEL_IMAGING_EXT_COMPUTATION_NAME_TOKENS                \
+    ((aggregatorComputation, "skinningInputAggregatorComputation")) \
+    ((computation,           "skinningComputation"))
+
+TF_DECLARE_PUBLIC_TOKENS(
+    UsdSkelImagingExtComputationNameTokens, USDSKELIMAGING_API,
+    USD_SKEL_IMAGING_EXT_COMPUTATION_NAME_TOKENS);
+
+#define USD_SKEL_IMAGING_EXT_AGGREGATOR_COMPUTATION_INPUT_NAME_TOKENS \
+    (restPoints)                                                      \
+    (geomBindXform)                                                   \
+    (influences)                                                      \
+    (numInfluencesPerComponent)                                       \
+    (hasConstantInfluences)                                           \
+    (blendShapeOffsets)                                               \
+    (blendShapeOffsetRanges)                                          \
+    (numBlendShapeOffsetRanges)
+
+TF_DECLARE_PUBLIC_TOKENS(
+    UsdSkelImagingExtAggregatorComputationInputNameTokens, USDSKELIMAGING_API,
+    USD_SKEL_IMAGING_EXT_AGGREGATOR_COMPUTATION_INPUT_NAME_TOKENS);
+
+#define USD_SKEL_IMAGING_EXT_COMPUTATION_INPUT_NAME_TOKENS \
+    (primWorldToLocal)                                     \
+    (blendShapeWeights)                                    \
+    (skinningXforms)                                       \
+    (skinningScaleXforms)                                  \
+    (skinningDualQuats)                                    \
+    (skelLocalToWorld)
+
+TF_DECLARE_PUBLIC_TOKENS(
+    UsdSkelImagingExtComputationInputNameTokens, USDSKELIMAGING_API,
+    USD_SKEL_IMAGING_EXT_COMPUTATION_INPUT_NAME_TOKENS);
+
+#define USD_SKEL_IMAGING_EXT_COMPUTATION_OUTPUT_NAME_TOKENS \
+    (skinnedPoints)                                    
+
+TF_DECLARE_PUBLIC_TOKENS(
+    UsdSkelImagingExtComputationOutputNameTokens, USDSKELIMAGING_API,
+    USD_SKEL_IMAGING_EXT_COMPUTATION_OUTPUT_NAME_TOKENS);
+
 PXR_NAMESPACE_CLOSE_SCOPE
 
 #endif // PXR_USD_IMAGING_USD_SKEL_IMAGING_TOKENS_H
