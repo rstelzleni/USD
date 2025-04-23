3bcfc9ae9764ea42dcd7471964a4fd23205edb1d
rajabala <rajabala@users.noreply.github.com>
2025-04-23 15:27:41 -0700

    [hdui] Add version header.
    (Internal change: 2364826)

---
+++ extras/imaging/examples/hdui/version.h
@@ -0,0 +1,12 @@
+//
+// Copyright 2025 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+#ifndef PXR_IMAGING_HDUI_VERSION_H
+#define PXR_IMAGING_HDUI_VERSION_H
+
+#define HDUI_API_VERSION 11
+
+#endif // PXR_IMAGING_HDUI_VERSION_H
