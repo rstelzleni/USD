8a446340fee77227a8d2c191497b02f3ffce4102
rajabala <rajabala@users.noreply.github.com>
2025-05-30 10:24:19 -0700

    [hdSt] Add scene index plugin to nullify unbound materials.
    (Internal change: 2368892)

---
+++ pxr/imaging/hdSt/unboundMaterialOverridingSceneIndexPlugin.cpp
@@ -0,0 +1,69 @@
+//
+// Copyright 2025 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+
+#include "pxr/imaging/hdSt/unboundMaterialOverridingSceneIndexPlugin.h"
+
+#include "pxr/imaging/hd/dataSourceTypeDefs.h"
+#include "pxr/imaging/hd/retainedDataSource.h"
+#include "pxr/imaging/hd/materialBindingsSchema.h"
+#include "pxr/imaging/hd/sceneIndexPluginRegistry.h"
+#include "pxr/imaging/hd/tokens.h"
+#include "pxr/imaging/hdsi/unboundMaterialOverridingSceneIndex.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+TF_DEFINE_PRIVATE_TOKENS(
+    _tokens,
+    ((sceneIndexPluginName, "HdSt_UnboundMaterialOverridingSceneIndexPlugin"))
+);
+
+static const char * const _pluginDisplayName = "GL";
+
+TF_REGISTRY_FUNCTION(TfType)
+{
+    HdSceneIndexPluginRegistry::Define<
+        HdSt_UnboundMaterialOverridingSceneIndexPlugin>();
+}
+
+TF_REGISTRY_FUNCTION(HdSceneIndexPlugin)
+{
+    // This scene index should be added *before*
+    // HdSt_DependencyForwardingSceneIndexPlugin (which currently uses 1000).
+    const HdSceneIndexPluginRegistry::InsertionPhase insertionPhase = 900;
+
+    const HdTokenArrayDataSourceHandle bindingPurposesDs =
+        HdRetainedTypedSampledDataSource<VtArray<TfToken>>::New(
+            VtArray<TfToken>({
+                HdTokens->preview,
+                HdMaterialBindingsSchemaTokens->allPurpose,
+            }));
+    
+    const HdContainerDataSourceHandle inputArgs =
+        HdRetainedContainerDataSource::New(
+            HdsiUnboundMaterialOverridingSceneIndexTokens->
+                materialBindingPurposes,
+            bindingPurposesDs);
+
+    HdSceneIndexPluginRegistry::GetInstance().RegisterSceneIndexForRenderer(
+        _pluginDisplayName,
+        _tokens->sceneIndexPluginName,
+        inputArgs,
+        insertionPhase,
+        HdSceneIndexPluginRegistry::InsertionOrderAtStart);
+}
+
+HdSt_UnboundMaterialOverridingSceneIndexPlugin::
+HdSt_UnboundMaterialOverridingSceneIndexPlugin() = default;
+
+HdSceneIndexBaseRefPtr
+HdSt_UnboundMaterialOverridingSceneIndexPlugin::_AppendSceneIndex(
+    const HdSceneIndexBaseRefPtr &inputScene,
+    const HdContainerDataSourceHandle &inputArgs)
+{
+    return HdsiUnboundMaterialOverridingSceneIndex::New(inputScene, inputArgs);
+}
+
+PXR_NAMESPACE_CLOSE_SCOPE
