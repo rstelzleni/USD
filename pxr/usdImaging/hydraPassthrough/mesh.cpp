af7d9cf14228defa7134f38efaa409dacc2cec30
Ryan Stelzleni <rstelzleni@gmail.com>
2025-08-05 19:36:54 -0700

    init drawing repr, almost ready to access actual data

--- pxr/usdImaging/hydraPassthrough/mesh.cpp
+++ pxr/usdImaging/hydraPassthrough/mesh.cpp
@@ -20,7 +20,42 @@ HdDirtyBits HdTinyMesh::_PropagateDirtyBits(HdDirtyBits bits) const {
   return bits;
 }
 
-void HdTinyMesh::_InitRepr(TfToken const &reprToken, HdDirtyBits *dirtyBits) {}
+void HdTinyMesh::_InitRepr(TfToken const &reprToken, HdDirtyBits *dirtyBits) {
+    std::cout << "=> InitRepr for Tiny Mesh id=" << GetId() << " reprToken="
+                << reprToken << " dirtyBits=" << *dirtyBits << std::endl;
+    
+    // Initialize the representation with a default Repr.
+    // An rprim owns a shared data object, and it also has potentially 
+    // multiple reprs. Each repr can create one or more draw items,  which
+    // can share that data. These need to be initialized in _InitRepr using
+    // the protected members _reprs and _sharedData.
+    _ReprVector::iterator it = std::find_if(_reprs.begin(), _reprs.end(),
+                                            _ReprComparator(reprToken));
+    bool isNew = it == _reprs.end();
+
+    if (isNew) {
+        std::cout << "Creating new repr for Tiny Mesh id=" << GetId()
+                  << " reprToken=" << reprToken << std::endl;
+
+        // add new repr
+        _reprs.emplace_back(reprToken, std::make_shared<HdRepr>());
+        HdReprSharedPtr &repr = _reprs.back().second;
+
+        // set dirty bit to say we need to sync a new repr (buffer array
+        // ranges may change)
+        *dirtyBits |= HdChangeTracker::DirtyRepr;
+
+        HdRepr::DrawItemUniquePtr drawItem =
+            std::make_unique<HdDrawItem>(&_sharedData);
+        //HdDrawingCoord *drawingCoord = drawItem->GetDrawingCoord();
+        repr->AddDrawItem(std::move(drawItem));
+
+    } else {
+        std::cout << "Repr already exists for Tiny Mesh id=" << GetId()
+                  << " reprToken=" << reprToken << std::endl;
+    }
+}
+
 
 void HdTinyMesh::Sync(HdSceneDelegate *sceneDelegate,
                       HdRenderParam *renderParam, HdDirtyBits *dirtyBits,
